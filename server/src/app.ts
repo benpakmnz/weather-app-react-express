31f9da0ee3065b6ec85d61146afdcd8abc08fe43
benny <benpakmnz@gmail.com>
2023-12-15 23:33:42 +0200

    deployment

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,29 +1,39 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import { json } from "body-parser";
 import { NotFoundError } from "./common/errors/not-found-error";
 import { weatherRouter } from "./routes/weather-routes";
 import { errorHandler } from "./middlewares/error-handler";
 import { authRouter } from "./routes/auth-routes";
+import path from "path";
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
 
 app.use(json());
+app.use(express.static(path.join("public")));
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", `${process.env.CLIENT_ENV}`);
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  next();
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+//   next();
+// });
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello");
 });
 
 app.use("/api/auth", authRouter);
 app.use("/api/weather", weatherRouter);
+
+app.use((req, res, next) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
